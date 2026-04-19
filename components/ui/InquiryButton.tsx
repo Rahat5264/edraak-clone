9876b8addc4e54929dba7e71bc9a4aa769f92c19
rahat5264 <rahatabbas154@gmail.com>
2026-04-19 22:19:39 +0500

    hide inquiry

--- components/ui/InquiryButton.tsx
+++ components/ui/InquiryButton.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 
+const DISABLE_INQUIRY = true
+
 type Props = {
   product?: any
   title?: string
@@ -10,6 +12,8 @@ type Props = {
 }
 
 export default function InquiryButton({ product, title, className, style }: Props) {
+  if (DISABLE_INQUIRY) return null
+
   const handle = (e: React.MouseEvent) => {
     e.preventDefault()
     const detail = { product: product || { title: title || '' } }
