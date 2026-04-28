7ec1bfe9dc0336885309cb2fcd1246d64b805baf
rahat5264 <rahatabbas154@gmail.com>
2026-04-28 13:42:57 +0500

    bug fixes

--- app/products/highspeed-barcode-seam-sensor/page.tsx
+++ app/products/highspeed-barcode-seam-sensor/page.tsx
@@ -84,6 +84,8 @@ export default function HighspeedBarcodePage() {
                 </div>
               )}
 
+              </div>
+
               <div className="mt-8 flex gap-3">
                 <Link href="/products" className="inline-block px-4 py-2 text-white" style={{ backgroundColor: 'rgb(5,3,42)', borderRadius: 0 }}>Back</Link>
                 <InquiryButton product={prod} className="inline-block px-4 py-2 text-white" style={{ backgroundColor: 'rgb(5,3,42)', borderRadius: 0 }} />
