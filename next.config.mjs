a1ce1475e99905ccdd8ed226f9a7692792e96229
rahat5264 <rahatabbas154@gmail.com>
2026-05-04 15:07:39 +0500

    url redirects .1

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,75 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/fabric-processing/width-measurement',
+        destination: '/products/width-measurement-system',
+        permanent: true,
+      },
+      {
+        source: '/hiring',
+        destination: '/career',
+        permanent: true,
+      },
+      {
+        source: '/industries/agritech',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/erp-systems',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/industries/pharma',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/fabric-processing',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/stitching-apparel/wash-monitoring-system',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/industries/agritech/visual-crop-analysis',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/posts/business-intelligence-quality-assurance-system',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/services/artificial-intelligence/nlp-genarative-ai',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/stitching-apparel/wages',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/posts/production-tracking-data-logging-recording-and-tracking',
+        destination: '/products/fabric-production-processing',
+        permanent: true,
+      },
+      {
+        source: '/industries/fabric-processing/production-tracking',
+        destination: '/products/fabric-production-processing',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
